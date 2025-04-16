631029ead0a376545fb21b32248354df4350e8e9
sitiqomariah08 <qomariasiti17@gmail.com>
2025-04-16 13:14:06 +0700

    Update koneksi.php

--- MWP78_webb/koneksi.php
+++ MWP78_webb/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=172.21.96.1 dbname=MWP user=postgres password=nidzom15");
+$conn = pg_connect("host= dbname= user=postgres password=123");
 
 if (!$conn) {
     die("Koneksi gagal: " . pg_last_error());
